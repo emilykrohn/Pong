9a415a9dbc1491b5e0c911a564eb2bdd07dacd30
Emily Krohn <emilykrohn2@gmail.com>
2024-07-04 07:39:09 -0700

    Initial commit

---
+++ Pong/Pong.cpp
@@ -0,0 +1,24 @@
+#include "raylib.h"
+
+int main()
+{
+	const int screenWidth = 800;
+	const int screenHeight = 450;
+
+	InitWindow(screenWidth, screenHeight, "Pong");
+
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+			
+			ClearBackground(RAYWHITE);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
+	return 0;
+}
