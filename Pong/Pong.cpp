e36f02b05b0f657fa4ae5744185a1aca2dfb0611
Emily Krohn <emilykrohn2@gmail.com>
2024-07-05 12:31:48 -0700

    Add Ball Movement

--- Pong/Pong.cpp
+++ Pong/Pong.cpp
@@ -1,19 +1,34 @@
 #include "raylib.h"
 
+const int screenWidth = 800;
+const int screenHeight = 450;
+
+class Ball {
+public:
+	Vector2 position;
+	Vector2 velocity;
+};
+
+Ball ball;
+
+void InitGame();
+void DrawGame();
+void DrawBall(int size, Color color);
+void MoveBall(int speed);
+
 int main()
 {
-	const int screenWidth = 800;
-	const int screenHeight = 450;
-
 	InitWindow(screenWidth, screenHeight, "Pong");
+
+	InitGame();
 
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
-			
-			ClearBackground(RAYWHITE);
+
+			DrawGame();
 
 		EndDrawing();
 	}
@@ -22,3 +37,28 @@ int main()
 
 	return 0;
 }
+
+void InitGame()
+{
+	ball.position = Vector2{ screenWidth / 2, screenHeight / 2 };
+	ball.velocity = Vector2{ 1, 1 };
+}
+
+void DrawGame()
+{
+	ClearBackground(RAYWHITE);
+	DrawBall(20, LIGHTGRAY);
+}
+
+void DrawBall(int size, Color color)
+{
+	DrawCircle(ball.position.x, ball.position.y, size, color);
+	MoveBall(2);
+}
+
+void MoveBall(int speed)
+{
+	ball.position.x += ball.velocity.x * speed;
+	ball.position.y += ball.velocity.y * speed;
+}
+
